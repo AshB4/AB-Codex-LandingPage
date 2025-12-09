86c1cb0d542a19fc1effd70f2a13e019ead6d08b
Ashley B <109474630+AshB4@users.noreply.github.com>
2025-12-08 21:44:50 -0600

    Add files via upload
    Added bootstrap and its pulling from supabase

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
 const supabaseUrl = "https://gnchphtvepgmduhidwmm.supabase.co";
 const apiKey =
 	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImduY2hwaHR2ZXBnbWR1aGlkd21tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3OTM3NzQsImV4cCI6MjA4MDM2OTc3NH0.IsMy2XTSSieE7N4Zn-WDS0pFN2Y1TIisP8JCcax8k0M"; 
+const prodsUrl = "${supabaseUrl}/rest/v1/products`";
 
 async function submitLead(event) {
-    console.log("submitLead IS RUNNING");
 	event.preventDefault();
 
 	const leadData = getLeadData();
@@ -36,3 +36,50 @@ function getLeadData() {
 }
 
 document.getElementById("contact-form").addEventListener("submit", submitLead);
+
+async function getProducts() {
+	const response = await fetch(supabaseUrl + "/rest/v1/products", {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+			apikey: apiKey,
+			Authorization: "Bearer " + apiKey,
+		},
+	});
+
+	const data = await response.json();
+
+	let prodsCards = document.getElementById("prodsCards");
+
+	for (let i = 0; i < data.length; i++) {
+  prodsCards.innerHTML += `
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3 d-flex justify-content-center mb-4">
+      <div class="card shadow-sm rounded-3 border-0" style="width: 18rem;">
+
+        <div class="ratio ratio-1x1">
+          <img 
+            src="${data[i].img_url}" 
+            alt="${data[i].name}" 
+            class="img-fluid object-fit-cover rounded-top"
+          >
+        </div>
+
+        <div class="card-body d-flex flex-column">
+          <h5 class="card-title text-center">${data[i].name}</h5>
+          <p class="card-text">${data[i].description}</p>
+          <p class="mt-auto fw-bold bg-primary text-white text-center py-2 rounded">
+  $${data[i].price}
+</p>
+        </div>
+
+      </div>
+    </div>
+  `;
+}
+}
+
+document.addEventListener("DOMContentLoaded", getProducts);
+
+	
+
+
